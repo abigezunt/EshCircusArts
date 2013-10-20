e0f5e2366f6547b92dfc9f0a316d6d4d7c7311ee
Abby Howell <abby.g.howell@gmail.com>
2013-10-20 17:49:20 -0400

    working on getting unpaid course balance into new charge form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ActiveRecord::Base
   has_many :courses, through: :registrations
+
+  def unpaid_balance
+  	unpaid_registrations = Registration.find(:all, :conditions => [:user_id == self.id, :paid == false])
+  	balance = 0
+  	unpaid_registrations.each do |registration|
+  		balance += registration.course.price
+  	end
+  	return balance
+  end
+
 end
