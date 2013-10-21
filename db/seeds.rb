4dad9897d2f014affecbfd04220234d572c2bf5c
Abby Howell <abby.g.howell@gmail.com>
2013-10-21 00:07:48 -0400

    working on seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Discipline.create(
+	name: "Contortion",
+	description: "This beginning contortion class focuses on developing flexibility in all parts of the back, and on improving split flexibility into oversplit flexibility.  Whether you want a to achieve a cheststand with feet flat on the floor, or work toward a full headsit, this class will give you the tools you need to reach your flexibility goals. \n \n
+		Pre-requisites: \n
+		* Comfortably hold a backbend for 1 minute (we will be upside down a lot)\n
+		* Backbend with arms and legs straight\n
+		* Elbowstand (against the wall or with spotter help is ok)\n
+		* Good flexibility in right and left side splits and pancake straddle"
+		photo_url: "instructors/Tracy.jpg")
+
+Discipline.create(
+	name: "Handbalancing",
+	description: "Do you dream of the perfect handstand? Whether your goal is improving your form or advanced tricks like: crocodiles, mexican handstands, and one arms, this is the class that will help get you there. Designed to give each student the individual attention of the coach, the group size is limited to six. Classes consist of a 10-15 minute warm up followed by an hour of skill work and conditioning. Form and technique are emphasized. You must be able to hold a 30 second handstand against a wall.",
+	level_3_description: "This class is for handbalancers who are looking for intermediate to advanced level instruction. \n Hand Balancing Level 3/4 pre-requisites: \n
+		* Minimum 1 session open level hand balancing or instructor permission
+		* 5 second free-standing handstand with straight arms, legs together, hollow body
+		* Solid 2-arm croc on canes with good form"
+	photo_url: "disciplines/handbalancing.jpg")
+
+Discipline.create(
+	name: "Partner Acrobatics",
+	description: "Partner acrobatics consists of a base supporting a flyer in a variety of poses, lifts, and dynamic movements.  Each class explores range of partner and group balancing poses and transitions.  Students will develop strength and technique, and focus on the “hows” of balancing, breaking down how each trick works, and how to experiment safely to develop new variations. No need to bring a partner- the whole class works together. Classes will also cover safe spotting techniques.",
+	level_1_description: " In this intro level class, we will explore a range of partner and group balancing poses and transitions. We will develop strength and technique, and focus on the “hows” of balancing, breaking down how each trick works. We will also learn safe spotting techniques. Each student will get to try basing, flying, and spotting. \n Students do not need to have experience with partner acrobatics, but we recommend that students are comfortable attempting to kick up to a handstand against a wall. No partner is required – the whole class will work together. Come and play!",
+	level_2_description: "Level 2 will build on the skills developed in Level 1, and will introduce students to intermediate level tricks and short sequences. We will begin to explore basic overhead lifts, and will start to play with more complex counterbalances. This level is appropriate for students who have some experience with partner balancing, either at EAA or at other schools. Students should be able to hold a 30 second handstand against a wall, and should be comfortable either basing or flying all of the following: thigh stands; stacks/two-highs (standing on shoulders); candlestick.",
+	level_3_description: "This level is for students who are comfortable with two-highs, inversions, and trying basic overhead lifts (with spotter help is ok!). Students in Level 3 will start to work on more advanced tricks such as shoulder-to-shoulder, hand-to-foot and hand-to-hand (with the base lying down), and basic drops. We will build larger group pyramids, and begin to play with improvisation and creating short sequences.",
+	level_4_description: "This advanced level class is for students who are comfortable either basing or flying overhead lifts and velocity tricks. Students in this level will begin to work on pitching, standing hand-to-hand and hand-to-foot, and more advanced lifts with the help of safety lines. We will continue to work on improvisation in pairs and groups, using the knowledge and movement vocabulary developed in previous levels.",
+	photo_url: "disciplines/partner-acro.jpg",
+	)
 
 Discipline.create(
 	name: "Trapeze", 
@@ -56,12 +77,7 @@ Discipline.create(
 		  * Familiarity and competence with momentum tricks such as russian rolls, milcircles, and front hip circles.\n
 		  * Comfort with single knee hangs and knee hang beats.\n
 		  * Familiarity with: front rolls, foot flags, vertical splits, drops onto the bar (lyra).\n
-		  * Has the strength and stamina to complete 2-minute sequences in the air.\n
-		", 
-	all_levels: "", 
-	apparatus_description: "", 
-	apparatus_photo_urls: ["", ""]
-	)
+		  * Has the strength and stamina to complete 2-minute sequences in the air.\n")
 
 Discipline.create(
 	name: "Aerial Hoop", 
@@ -110,26 +126,115 @@ Discipline.create(
 		  * Familiarity and competence with momentum tricks such as russian rolls, milcircles, and front hip circles.\n
 		  * Comfort with single knee hangs and knee hang beats.\n
 		  * Familiarity with: front rolls, foot flags, vertical splits, drops onto the bar (lyra).\n
-		  * Has the strength and stamina to complete 2-minute sequences in the air.\n
-		", 
-	all_levels: "", 
-	apparatus_description: "", 
-	apparatus_photo_urls: ["", ""]
+		  * Has the strength and stamina to complete 2-minute sequences in the air."
 	)
 
-# Discipline.create(
-# 	name: "", 
-# 	description: "", 
-# 	photo_url: "", 
-# 	photo_caption: "", 
-# 	level_1_description: "", 
-# 	level_2_description: "", 
-# 	level_3_description: "", 
-# 	level_4_description: "", 
-# 	all_levels: "", 
-# 	apparatus_description: "", 
-# 	apparatus_photo_urls: ["", ""]
-# 	)
+Discipline.create(
+	name: "Aerial Silks", 
+	description: "", 
+	photo_url: "disciplines/silks.jpg", 
+	photo_caption: "", 
+	level_1_description: 
+
+		"This is for people who are either brand new (in their first-ever class or set of classes) or who are learning how to get comfortable on their apparatuses. People in this level set are working on skills like climbing the silks and wrapping foot locks. People in this level will work on inversion strength, body awareness, and comfort in the air. Students may spend a couple of sessions at this level while the build basic core and upper body strength.  \n \n Power classes are offered in multiple disciplines, and are aimed at those students who do not necessarily have specific experience in the discipline being taught, but who have applicable strength and/or flexibility from a background in a related physical discipline (gymnastics, dance, rock climbing, etc.)  If you are unsure whether a power class is appropriate for you, please contact us at scheduling@eshaerialarts.com with a specific description of your background.", 
+	level_2_description: 
+
+		"Generally, this is for students who have completed some course of aerial training either at EAA or at other schools, and have a baseline of core/upper body strength. This level will begin to work with more complex wraps, poses, and transitions. Silks students will begin to learn basic drops at this level. \n \n
+		Pre-requisites: \n
+		* Ability to climb safely with good shoulder form\n
+		* Ability to foot lock in the air\n
+		* Ability to hang in wrist locks with shoulders and abs engaged\n
+		* Ability to invert from the mat without instructor’s assistance (jumping is ok!)\n
+		* Understanding of good shoulder alignment both upright and inverted\n
+		* Familiarity with: double crochet, double foot lock splits, single-foot roll up, arabesque, beginning to work on hip keys", 
+	level_3_description: 
+
+	"This level is for students who are comfortable with inversions, hip keys, same-side knee hooks, and crochets. Students in this level will begin to work on movements higher in the air, as well as sequences to build stamina, more advanced drops, and some choreographic skills. Students should be able to invert from the mat with minimal jumping, or be able to invert in the air (using feet and/or swinging to help you up is ok!). \n \n
+		Pre-requisites: \n
+ 		* Ability to invert in the air (bent arms, some beat to get up is ok).\n
+ 		* Ability to hip key cleanly on the ground, and competently in the air (in-air hip key does not have to be perfect).\n
+ 		* Ability to execute forward and reverse foot locks, en dedans wraps, and half locks.\n
+ 		* Ability to execute cross-back straddle without help from instructor.\n
+ 		* Ability to hang from hands without wrist locks for at least ten seconds, arms both straight overhead and bent, with good shoulder engagement.
+		* Familiarity with basic drops such as single star and front salto.", 
+	level_4_description: 
+	"This is for advanced students who are comfortable with being in the air on their chosen apparatus. \n
+		Classes labeled with “continuing” are for students who have been at Level 4 for a couple of sessions; these classes require that students already be familiar with Level 4 vocabulary. \n
+		These classes will start to work on dynamic movements and unlocked drops/release moves, and will encourage students to find creative transitions and even invent new tricks, shapes, and movements of their own. As with all other levels, technique and injury prevention will continue to be emphasized.\n \n
+		Pre-requisites: \n
+		* Ability to invert in the air cleanly and repeatedly (no beat, working towards straight arms).\n
+		* Able to execute multiple consecutive opposite-side and same-side climbs.\n
+		* Competence with slack drops, hip key drops, double star drops and 1.5 drops.\n
+		* Ability to execute clean in-air hip keys.\n
+		* Has the strength and stamina to complete 2-minute sequences on the fabrics."
+	)
+
+
+  Course.create(
+  	name: "Handstands 101 with Roger May"
+  	start_date: "Oct 26, 2013", 
+  	number_of_sessions: 1, 
+  	price: 40.00, 
+  	description: "Come and invert!
+	In this two hour workshop, students will learn the fundamentals of handstands: proper shoulder form, body alignment, and the techniques to find safe, steady, solid control when upside down.  No handstand or acrobatic background required - beginners welcome!  This workshop is open to all students ages 16+.",
+	start_time: "2:30 pm"
+	end_time: "4:30 pm",
+	level: "all levels",
+	)
+
+  Course.create(
+  	name: "Stretching and Conditioning for Aerial Artists with Trevor Kafka",
+  	start_date: "nov 16, 2013",
+  	number_of_sessions: 1,
+  	price: 50.00,
+  	start_time: "2:30 pm",
+  	end_time: "5:30 pm"
+  	description: "In this workshop we will explore techniques and exercises to get the most out of your aerial training.  We will work through a progression of ground-based and aerial skills designed to balance strength, flexibility, and safety. We'll cover effective warm-ups, conditioning, stretching, and tips for structuring your training time.  Pre-requisites:  Successful completion of at least one session of Level 1 Silks or Level 1 Hoop/Trapeze at Esh, or equivalent training at another circus school.  To ensure everyone's safety during the workshop, pre-requisites will be enforced."
+  	level: "2"
+  	)
+
+  Course.create(
+  	name: "Point Your Toes! with Ellen May",
+  	start_date: "Dec 7th, 2013",
+  	number_of_sessions: 1,
+  	price: 40.00,
+  	start_time: "2:30 pm",
+  	end_time: "4:30 pm",
+  	description: "Pointed toes, straight legs, and extended arms don't just make your skills look better - they make them easier, too! Come learn how to lift, lengthen, and engage your muscles both in the air and on the ground with a series of drills and games designed to build strength and full body awareness. This workshop is appropriate for ground and aerial acrobats and students of all levels, and has no prerequisites.  Ages 16+ or permission of instructor.",
+  	level: "all levels"
+  	)
+
+  Course.create(
+  	name: "Aerial Taster Class",
+  	start_date: "Oct 27, 2013",
+  	start_time: "4:00 pm",
+  	end_time: "5:30 pm",
+  	number_of_sessions: 1,
+  	price: 25.00,
+  	description: "Want to learn to fly? Want variation in your exercise regimen? Just want to have fun? Circus classes focus on form and body awareness through skill work, with an emphasis on strength building and injury prevention.  Aerial Taster classes give students a chance to try several of the aerial arts offered at Esh before committing to a full session of classes.  Students will have the chance to try aerial fabric (aka silks), aerial rope, static trapeze, and aerial hoop (aka lyra).  No experience or specific athletic ability is necessary- all levels are welcome! If you love the Taster Class and decide to register for a new, full session aerial course, we'll deduct the cost of the Taster Class from the session fee!  You must register for the session course before leaving the studio at the end of the Taster Class to receive the discount.  The discount is only available for new students, or for returning students who are trying a new discipline for the first time."
+  	)
+
+  Course.create(
+  	name: "Aerial Taster Class",
+  	start_date: "Nov 3, 2013",
+  	start_time: "11:30 am",
+  	end_time: "1:00 pm",
+  	number_of_sessions: 1,
+  	price: 25.00,
+  	description: "Want to learn to fly? Want variation in your exercise regimen? Just want to have fun? Circus classes focus on form and body awareness through skill work, with an emphasis on strength building and injury prevention.  Aerial Taster classes give students a chance to try several of the aerial arts offered at Esh before committing to a full session of classes.  Students will have the chance to try aerial fabric (aka silks), aerial rope, static trapeze, and aerial hoop (aka lyra).  No experience or specific athletic ability is necessary- all levels are welcome! If you love the Taster Class and decide to register for a new, full session aerial course, we'll deduct the cost of the Taster Class from the session fee!  You must register for the session course before leaving the studio at the end of the Taster Class to receive the discount.  The discount is only available for new students, or for returning students who are trying a new discipline for the first time."
+  	)
+
+Course.create(
+  	name: "Partner Acro and Handbalancing Taster Class",
+  	start_date: "Nov 2, 2013",
+  	start_time: "2:30 pm",
+  	end_time: "4:00 pm",
+  	number_of_sessions: 1,
+  	price: 15.00,
+  	description: "Want to learn to fly? Want variation in your exercise regimen? Just want to have fun? Circus classes focus on form and body awareness through skill work, with an emphasis on strength building and injury prevention.  Aerial Taster classes give students a chance to try several of the aerial arts offered at Esh before committing to a full session of classes.  Students will have the chance to try aerial fabric (aka silks), aerial rope, static trapeze, and aerial hoop (aka lyra).  No experience or specific athletic ability is necessary- all levels are welcome! If you love the Taster Class and decide to register for a new, full session aerial course, we'll deduct the cost of the Taster Class from the session fee!  You must register for the session course before leaving the studio at the end of the Taster Class to receive the discount.  The discount is only available for new students, or for returning students who are trying a new discipline for the first time."
+  	)
+
+
 
 User.create(
 	name: "Rachel Stewart", 
