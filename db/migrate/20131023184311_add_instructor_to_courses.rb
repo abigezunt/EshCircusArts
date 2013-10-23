d026a32d5269027df691ff48d0ff03fc02e37c70
Abby Howell <abby.g.howell@gmail.com>
2013-10-23 14:43:59 -0400

    generated migrations to update database

---
+++ db/migrate/20131023184311_add_instructor_to_courses.rb
@@ -0,0 +1,4 @@
+class AddInstructorToCourses < ActiveRecord::Migration
+  def change
+  end
+end
---
+++ db/migrate/20131023184336_add_role_to_course_registrations.rb
@@ -0,0 +1,4 @@
+class AddRoleToCourseRegistrations < ActiveRecord::Migration
+  def change
+  end
+end
