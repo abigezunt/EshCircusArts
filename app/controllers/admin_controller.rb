9d3ea4d5cf5c71c00e9781dffa99ce3a8b21a432
Abby Howell <abby.g.howell@gmail.com>
2013-10-24 12:42:13 -0400

    drafting out admin-only views

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,5 +2,48 @@ class AdminController < ApplicationController
 
   def admin_index
   end
-  
+
+  def users_index
+  end
+
+  def user_show
+  end
+
+  def user_edit
+  end
+
+  def instructor_new
+  end
+
+  def instructor_create
+  end
+
+  def course_registrations_index
+  end
+
+  def course_registrations_show
+  end
+
+  def courses_index
+  end
+
+  def courses_show
+  end
+
+  def courses_edit
+  end
+
+  def course_new
+  end
+
+  def course_create
+  end
+
+  def course_destroy
+  end
+
+
+
+
+
 end
