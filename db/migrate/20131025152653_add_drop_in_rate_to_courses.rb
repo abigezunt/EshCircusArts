b680dcfc8f26e749e7f325b47f6e575ea438b175
Abby Howell <abby.g.howell@gmail.com>
2013-10-25 11:29:29 -0400

    added drop_in_rate to courses and sessions

--- db/migrate/20131025152653_add_drop_in_rate_to_courses.rb
+++ db/migrate/20131025152653_add_drop_in_rate_to_courses.rb
@@ -1,4 +1,5 @@
 class AddDropInRateToCourses < ActiveRecord::Migration
   def change
+  	add_column :courses, :drop_in_rate, :integer
   end
 end
--- db/migrate/20131025152713_add_drop_in_rate_to_seven_week_sessions.rb
+++ db/migrate/20131025152713_add_drop_in_rate_to_seven_week_sessions.rb
@@ -1,4 +1,5 @@
 class AddDropInRateToSevenWeekSessions < ActiveRecord::Migration
   def change
+  	add_column :seven_week_sessions, :drop_in_rate, :integer
   end
 end
